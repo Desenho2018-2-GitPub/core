9079761c81c17acaab062c73d6d079ac7153c693
Vitor Falcão <vitorfhcosta@gmail.com>
2018-10-05 09:25:31 -0300

    Fix docker to web wait db connection

---
+++ gitpub/scripts/wait_db.py
@@ -0,0 +1,29 @@
+import psycopg2
+import time
+import os
+
+pg_user = 'postgres'
+pg_psw = 'postgres'
+pg_db = 'postgres'
+pg_host = 'db'
+
+
+start = time.time()
+success = False
+param = "dbname='{}' user='{}' host='{}' password='{}'".format(pg_db, pg_user, pg_host, pg_psw)
+
+while True:
+    try:
+        db = psycopg2.connect(param)
+        success = True
+        break
+    except:
+        print('No DB connection')
+    end = time.time()
+    if not success and end - start >= 15:
+        print('ERROR: request timeout of 15 seconds')
+        exit(1)
+    time.sleep(1)
+    print('Trying again')
+
+print('Success on DB connection')
