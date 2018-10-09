5afc3586f89ab5aba5f2b0481b160642d1e05fbb
Vitor Falcão <vitorfhcosta@gmail.com>
2018-10-09 09:29:52 -0300

    Fix init_dev script

--- gitpub/scripts/init_dev.sh
+++ gitpub/scripts/init_dev.sh
@@ -3,5 +3,8 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-echo "** Starting application **"
-python3 /gitpub/scripts/wait_db.py && python3 manage.py runserver 0.0.0.0:8080
+echo "** Waiting for DB **"
+python3 /gitpub/scripts/wait_db.py || exit 1
+
+echo "Django App starting..."
+python3 manage.py runserver 0.0.0.0:8080
