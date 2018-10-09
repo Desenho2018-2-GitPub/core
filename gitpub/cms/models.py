afcad6657a5480d3cb9c52bf679fa91b9145e738
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-10-09 12:21:47 -0300

    Add User as abstract class for Student, Professor and Admin

--- gitpub/cms/models.py
+++ gitpub/cms/models.py
@@ -1,23 +1,53 @@
-from django.db import models
+"""
+Models module
+"""
 import datetime
+from django.db import models
+
 
 class User(models.Model):
     """
-    A user can be a student, professor, guest or a admin
+    Abstract class for users can be a student, professor or a admin
     """
-    username = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    is_admin = models.BooleanField(default=False)
-    is_teacher = models.BooleanField(default=False)
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=150)
+    password = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
-        return self.username
+        return self.name
+
+
+class Professor(User):
+    """
+    Professor
+    """
+    #  Professor's registry at University
+    registry = models.IntegerField()
+
+
+class Student(User):
+    """
+    Student
+    """
+    #  Professor's registry at University
+    registry = models.IntegerField()
+
+
+class Admin(User):
+    """
+    Admin makes users professors
+    """
+    pass
 
 class Material(models.Model):
     """
     Materials belong to projects
     """
     url = models.CharField(max_length=140)
+
 
 class Period(models.Model):
     """
@@ -36,6 +66,7 @@ class Period(models.Model):
     def __str__(self):
         return ", ".join([self.semester, self.year])
 
+
 class Course(models.Model):
     """
     A course is created by an user and belongs to a class
@@ -47,6 +78,7 @@ class Course(models.Model):
     def __str__(self):
         return self.name
 
+
 class Classroom(models.Model):
     """
     A classroom can be created and/or enrolled by an user
@@ -54,7 +86,8 @@ class Classroom(models.Model):
     """
     name = models.CharField(max_length=50)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_user')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,
+                              related_name='owner_user')
     enrolled_user = models.ManyToManyField(User)
     period = models.ForeignKey(Period, on_delete=models.CASCADE)
 
@@ -63,6 +96,7 @@ class Classroom(models.Model):
 
     class Meta:
         ordering = ('course', 'period', 'name')
+
 
 class Project(models.Model):
     """
@@ -75,6 +109,7 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Comment(models.Model):
     """
