666f5159bb602f1ef7712f9ae94054117731b2ec
Victor Moura <victor_cmoura@hotmail.com>
2018-10-09 11:54:01 -0300

    Adds logs and fixes python buffered log to stdout

--- gitpub/cms/views.py
+++ gitpub/cms/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gitpub.logging import debug
 
 # Create your views here.
+@debug
 def index(request):
     return render(request, 'index.html')
