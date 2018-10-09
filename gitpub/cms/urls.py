b0918d0bb129abdcef4fed6037bcabb95e697a71
Victor Moura <victor_cmoura@hotmail.com>
2018-10-09 13:35:34 -0300

    Fixes login and register urls

--- gitpub/cms/urls.py
+++ gitpub/cms/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^login/', views.login, name='login'),
-    url(r'^register/', views.register, name='register'),
+    url(r'^login', views.login, name='login'),
+    url(r'^register', views.register, name='register'),
     url(r'', views.index, name='index')
 ]
