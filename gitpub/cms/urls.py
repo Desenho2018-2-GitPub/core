5c315a9db30b3786efbef7bccfcc86123a56fa7b
joao4018 <joao4018@gmail.com>
2018-11-09 00:59:40 -0200

    Add create user

--- gitpub/cms/urls.py
+++ gitpub/cms/urls.py
@@ -3,8 +3,14 @@ from .views import cms
 from .url_patterns import courses
 from django.conf.urls import url
 from django.urls import include
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from cms.core import views as core_views
 
 urlpatterns = [
     url(r'^$', cms.index, name='index'),
-    url(r'^courses/', include(courses))
+    url(r'^courses/', include(courses)),
+    url(r'^signup/$', core_views.signup, name='signup'),
+
 ]
