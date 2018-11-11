d3dad309b14b64b7389ba5c935bf3f4e9269fc35
Gabriel Ziegler <gabrielziegler3@gmail.com>
2018-11-11 15:56:24 -0200

    Fix user registration with blank username

--- gitpub/cms/forms.py
+++ gitpub/cms/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from cms.models import *
+from cms.models import RegisteredUser
 
 
 class SignUpForm(UserCreationForm):
-   name = forms.CharField(max_length=150, required=False, help_text='Optional.')
-   registry = forms.IntegerField(required=False, help_text='Optional.')
-   email = forms.CharField(max_length=150, help_text='Required. Inform a valid email address.')
+   name = forms.CharField(max_length=120, required=True, help_text='Full name')
+   username = forms.CharField(max_length=150, required=True, help_text='Username')
+   registry = forms.IntegerField(required=True, help_text='University Registry')
+   email = forms.CharField(max_length=150, required=True, help_text='Required. Inform a valid email address.')
 
    class Meta:
       model = RegisteredUser
-      fields = ('email', 'name', 'registry', ) 
+      fields = ('email', 'username', 'name', 'registry', 'password1', 'password2')
--- gitpub/cms/views/users.py
+++ gitpub/cms/views/users.py
@@ -17,7 +17,7 @@ def signup(request):
            raw_password = form.cleaned_data.get('password1')
            user = authenticate(username=username, password=raw_password)
            login(request, user)
-           return redirect('home')
+           return redirect('/')
    else:
        form = SignUpForm()
    return render(request, 'signup.html', {'form': form})
