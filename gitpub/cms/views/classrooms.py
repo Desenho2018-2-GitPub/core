0730429180983e2e2d84837f5f21e8836c4a280c
Victor Moura <victor_cmoura@hotmail.com>
2018-11-16 21:13:34 -0200

    Adds required login to all views in classrooms

--- gitpub/cms/views/classrooms.py
+++ gitpub/cms/views/classrooms.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from gitpub.logging import debug
 from cms.models import Classroom, Course, RegisteredUser, Period
 from django.core.exceptions import MultipleObjectsReturned
+from django.contrib.auth.decorators import login_required
 
 # GET /courses/course_id/classrooms
 @debug
+@login_required(login_url='/login')
 def index(request, course_id):
     course = get_object_or_404(Course, id=course_id)
     classrooms = Classroom.objects.filter(course__id=course_id)
@@ -17,6 +19,7 @@ def index(request, course_id):
 
 # GET /courses/course_id/classrooms/classroom_id
 @debug
+@login_required(login_url='/login')
 def show(request, course_id, classroom_id):
     classroom = get_object_or_404(Classroom, id=classroom_id)
     data = {
@@ -28,6 +31,7 @@ def show(request, course_id, classroom_id):
 
 # GET /courses/course_id/classrooms/new
 @debug
+@login_required(login_url='/login')
 def new(request, course_id):
     data = {
         'course_id': course_id
@@ -36,6 +40,7 @@ def new(request, course_id):
 
 # POST /courses/course_id/classrooms/create
 @debug
+@login_required(login_url='/login')
 def create(request, course_id):
     classroom_owner = request.user
     try:
@@ -59,12 +64,14 @@ def create(request, course_id):
 
 # GET /courses/course_id/classrooms/edit/classroom_id
 @debug
+@login_required(login_url='/login')
 def edit(request, course_id, classroom_id):
     classroom = get_object_or_404(Classroom, id=classroom_id)
     return render(request, 'classrooms/edit.html', {'classroom': classroom, 'course_id': course_id })
 
 # POST /classrooms/update
 @debug
+@login_required(login_url='/login')
 def update(request, course_id):
     classroom = get_object_or_404(Classroom, id=request.POST['classroom_id'])
     classroom.name = request.POST['classroom_name']
@@ -80,6 +87,7 @@ def update(request, course_id):
 
 # GET /courses/course_id/classrooms/delete/classroom_id
 @debug
+@login_required(login_url='/login')
 def delete(request, course_id, classroom_id):
     classroom = get_object_or_404(Classroom, id=classroom_id)
     classroom.delete()
