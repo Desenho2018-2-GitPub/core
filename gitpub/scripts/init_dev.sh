747d01c4a88aa4338dbf6f5628f8554e007474d8
Vitor Falcao <vitorfhcosta@gmail.com>
2018-11-17 12:50:19 -0200

    Run autopep8 in dev start

--- gitpub/scripts/init_dev.sh
+++ gitpub/scripts/init_dev.sh
@@ -8,6 +8,9 @@ python3 manage.py makemigrations
 echo "** Migrating changes if existent **"
 python3 manage.py migrate
 
+echo "** Running autopep8 **"
+autopep8 -r --in-place --agressive .
+
 echo "** Waiting for DB **"
 python3 /gitpub/scripts/wait_db.py || exit 1
 
