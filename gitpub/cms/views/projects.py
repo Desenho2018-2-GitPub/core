08f226e24e9b18103ba2328d63ffd0fb1de030fd
Victor Moura <victor_cmoura@hotmail.com>
2018-11-18 12:52:24 -0200

    Adds create project feature

--- gitpub/cms/views/projects.py
+++ gitpub/cms/views/projects.py
@@ -1,18 +1,36 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from gitpub.logging import debug
-from cms.models import Course, Classroom
+from cms.models import Classroom, Project
 from django.contrib.auth.decorators import login_required
 
 @debug
 @login_required(login_url='/login')
 def new(request, course_id, classroom_id):
-    return HttpResponse("NEW")
+    return render(request, 'projects/new.html', {'course_id': course_id, 'classroom_id': classroom_id})
 
 @debug
 @login_required(login_url='/login')
 def create(request, course_id, classroom_id):
-    pass
+    name = request.POST.get('name')
+    description = request.POST.get('description')
+
+    classroom = Classroom.objects.get(id=classroom_id)
+
+    project = Project(
+        name=name,
+        description=description,
+    )
+
+    project.save()
+
+    project.classroom.set([classroom])
+
+    project.save()
+
+    redirect_url = '/courses/{0}/classrooms/{1}'.format(course_id, classroom_id)
+
+    return redirect(redirect_url)
 
 @debug
 @login_required(login_url='/login')
